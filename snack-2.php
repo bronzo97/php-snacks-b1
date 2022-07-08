909b33caa109b876717eb65c7f51f557b3eb3b66
bronzo97 <danilobronzo97@hotmail.it>
2022-07-08 17:55:42 +0200

    started snack 2

---
+++ snack-2.php
@@ -0,0 +1,28 @@
+<?php
+
+$userName = $_GET['userName'];
+$userAge = $_GET['userAge'];
+$email = $_GET['email'];
+
+if(!$userName && $userAge && $email) {
+    echo "Access Denied";
+} else {
+
+$access = true;
+
+if (count($userName) < 3 ) {
+    $access = false;
+}
+
+if (is_numeric($userAge)) {
+    $access = false;
+}
+
+if (strpos($email, '@' && '.') ) {
+
+}
+
+
+}
+?>
+
