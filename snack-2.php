53a9d56eb1044bdb5b4f6563890ded0908f2265c
bronzo97 <danilobronzo97@hotmail.it>
2022-07-11 10:51:06 +0200

    updates

--- snack-2.php
+++ snack-2.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 $userName = $_GET['userName'];
 $userAge = $_GET['userAge'];
 $email = $_GET['email'];
@@ -29,5 +31,29 @@ if ($access === true) {
 }
 ?>
 
+<?php
+$name = key_exists('name', $_GET) ? $_GET['name'] : '';
+$mail = key_exists('mail', $_GET) ? $_GET['mail'] : '';
+$age = key_exists('age', $_GET) ? $_GET['age'] : '';
 
+$nameValid = false;
+$mailValid = false;
+$ageValid = false;
+$dataValid = false;
 
+if (strlen($name) > 3) {
+    $nameValid = true;
+}
+// strstr torna la stringa che abbiamo cercato, altrimenti torna false
+// possiamo usare anche strpos
+if (strpos($mail, '@') > 2 && strpos($mail, '.') > 4) {
+    $mailValid = true;
+}
+// 
+if (is_numeric($age)) {
+    $ageValid = true;
+}
+
+$dataValid = $nameValid && $mailValid && $ageValid;
+
+?>
